29b0bba04e41628b72241d3368b883c343aedf69
linlian <linlian@magefitness.com>
2019-09-18 16:01:27 +0800

    添加包的引用 例如english_words  https://pub.dev/packages/english_words

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,6 +7,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    final wordPair = new WordPair.random();
+
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -13,7 +17,8 @@ class MyApp extends StatelessWidget {
           title: new Text('Welcome to XXXX'),
         ),
         body: new Center(
-          child: new Text('Hello World'),
+//          child: new Text('Hello World'),
+        child: new Text(wordPair.asPascalCase),
         ),
       ),
     );
