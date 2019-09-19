6cd5c9c080a36b4541e4752e815e7f400f6ba84c
linlian <linlian@magefitness.com>
2019-09-19 15:01:14 +0800

    添加点击 收藏红心

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,7 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>(); //收藏的选项
 
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
@@ -74,11 +75,26 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: (){
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
